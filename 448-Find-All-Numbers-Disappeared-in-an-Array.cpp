b422888910e4a7333fbb8ad39b74387b09028c7f
Nagasai Challa <88236266+Nagasai-challa@users.noreply.github.com>
2024-12-28 10:43:07 +0530

    [448] [Time Beats: 12.05%] [Memory Beats: 9.90%] - LeetPush

---
+++ 448-Find-All-Numbers-Disappeared-in-an-Array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+       vector<int>ans;
+       set<int>s;
+       for(int i=0;i<nums.size();i++)
+       {
+        s.insert(nums[i]);
+       }
+       for(int i=1;i<=nums.size();i++)
+       {
+        if(s.find(i)==s.end()){
+            ans.push_back(i);
+        }
+       }
+       return ans;
+    }
+};
